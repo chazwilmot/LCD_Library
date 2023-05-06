0f9b05a20f85105b68b8c77202b7e466938ab7ab
Charles Wilmot II <69732849+chazwilmot@users.noreply.github.com>
2023-05-06 11:53:50 -0500

    Update defines.h

--- defines.h
+++ defines.h
@@ -1,4 +1,11 @@
 /*
+ ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * <chazwilmot@gmail.com> & <tristjpawlenty@gmail.com> wrote and adapted this file.  
+ * As long as you retain this notice you can do whatever you want with this stuff. 
+ * If we meet some day, and you think this stuff is worth it, you can buy me a beer.
+ *                                          Charles Wilmot and Tristan Pawlenty
+ * ----------------------------------------------------------------------------
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
@@ -8,21 +15,23 @@
  *
  * General stdiodemo defines
  *
+ * Replace X and Y with your defintions based on your hardware
+ *
  * $Id$
  */
 
 /* CPU frequency */
-#define F_CPU 1000000UL
+#define F_CPU X   // define your CPU Frequency
 
 /* UART baud rate */
-#define UART_BAUD  9600
+#define UART_BAUD  X   // define your UART baud rate
 
 /* HD44780 LCD port connections */
-#define HD44780_RS A, 6
-#define HD44780_RW A, 4
-#define HD44780_E  A, 5
+#define HD44780_RS X, Y   // X = PORT Name | Y = PORT number
+#define HD44780_RW X, Y   // X = PORT Name | Y = PORT number
+#define HD44780_E  X, Y   // X = PORT Name | Y = PORT number
 /* The data bits have to be not only in ascending order but also consecutive. */
-#define HD44780_D4 A, 0
+#define HD44780_D4 X, Y   // X = PORT Name | Y = PORT number
 
 /* Whether to read the busy flag, or fall back to
    worst-time delays. */
