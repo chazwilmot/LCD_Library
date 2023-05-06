f5c913c6aa311e9cd41328fd77769bab09de7f6c
Charles Wilmot II <69732849+chazwilmot@users.noreply.github.com>
2023-05-06 11:55:19 -0500

    Update hd44780.c

--- hd44780.c
+++ hd44780.c
@@ -1,4 +1,11 @@
 /*
+ ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * <chazwilmot@gmail.com> & <tristjpawlenty@gmail.com> wrote and adapted this file.  
+ * As long as you retain this notice you can do whatever you want with this stuff. 
+ * If we meet some day, and you think this stuff is worth it, you can buy me a beer.
+ *                                          Charles Wilmot and Tristan Pawlenty
+ * ----------------------------------------------------------------------------
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
