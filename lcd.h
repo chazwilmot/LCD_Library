298edac3dcee80f82ca289e065758ed4394218cb
Charles Wilmot II <69732849+chazwilmot@users.noreply.github.com>
2023-05-06 11:59:53 -0500

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,4 +1,11 @@
 /*
+ ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 43):
+ * <chazwilmot@gmail.com> & <tristjpawlenty@gmail.com> wrote and adapted this file.  
+ * As long as you retain this notice you can do whatever you want with this stuff. 
+ * If we meet some day, and you think this stuff is worth it, you can buy me a beer.
+ *                                          Charles Wilmot and Tristan Pawlenty
+ * ----------------------------------------------------------------------------
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
@@ -20,3 +27,19 @@ void	lcd_init(void);
  * Send one character to the LCD.
  */
 int	lcd_putchar(char c, FILE *stream);
+
+/*
+Returns the cursor to the start of the LCD 
+Column 0 Row 0
+*/
+void home(void);
+
+/*
+Clears the screen of the LCD and returns the cursor home
+*/
+void clear(void);
+
+/*
+Puts the cursor on the second row 
+*/
+void row2(void);
